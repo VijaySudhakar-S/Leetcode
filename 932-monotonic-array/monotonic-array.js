14080914aeb6241efc731ef20ba1a51894220e56
Vijay Sudhakar <161145790+VijaySudhakar-S@users.noreply.github.com>
2025-06-11 11:52:23 +0530

    Time: 4 ms (32.26%) | Memory: 67.3 MB (19.56%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let inc = true
+    let dec = true
+    for(let i=1;i<nums.length;i++){
+        if (nums[i] > nums[i-1]){
+            dec = false
+        }else if (nums[i] < nums[i-1]){
+            inc = false
+        }
+    }
+    return inc || dec
+};
