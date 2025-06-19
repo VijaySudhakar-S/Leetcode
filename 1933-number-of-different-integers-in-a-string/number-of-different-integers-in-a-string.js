72976639ff46bfecffd265c1135e8a1656e7047c
Vijay Sudhakar <161145790+VijaySudhakar-S@users.noreply.github.com>
2025-06-19 10:55:37 +0530

    Time: 8 ms (9.62%) | Memory: 53.7 MB (86.54%) - LeetSync

--- 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.js
+++ 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.js
@@ -7,7 +7,7 @@ var numDifferentIntegers = function(word) {
     if(nums == null) return 0
     let set = new Set()
     nums.forEach((n)=>{
-        set.add(BigInt(n).toString())
+        set.add(n.replace(/^0+/,""))
     })
     console.log(nums)
     console.log(set)
